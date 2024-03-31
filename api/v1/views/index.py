9c0a562ec69fba59dd29ef30733d951e6fb8a510
Your Name <you@example.com>
2024-03-31 14:42:52 +0200

    task3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,26 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """ returns status """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count_class():
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    obj_dic = {}
+    for key in classes:
+        obj_dic[classes[key]] = storage.count(key)
+        return jsonify(obj_dic)
