f4b05bc6a8abb45070bb4370ff89354d96f6d4dc
Your Name <you@example.com>
2024-03-31 02:35:41 +0200

    task3

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,17 +12,17 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(close):
     return storage.close()
 
 
-if __name__ == "__main__":
-    if getenv("HBNB_API_HOST"):
-        HBNB_API_HOST = getenv("HBNB_API_HOST")
-    else:
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
         HBNB_API_HOST = '0.0.0.0'
-    if getenv("HBNB_API_PORT"):
-        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
     else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
         HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
