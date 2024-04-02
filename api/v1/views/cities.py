5e35ce454ed109c192ddfba44e33f2ef0c4cd406
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-04-02 22:49:34 +0200

    CITIES VIEW

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -52,26 +52,20 @@ def delete_city(city_id):
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
-def post_city(state_id):
-    """
-    Creating a City
-    """
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
-
-    data = request.get_json()
-    instance = City(**data)
-    instance.state_id = state.id
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+def post_city():
+    """Create a `city` object"""
+    if not request.is_json:
+        abort(400, 'Request must be JSON')
+    if 'name' not in request.json:
+        abort(400, 'Missing name')
+    new_city = City(**request.json)
+    storage.new(new_city)
+    storage.save()
+    return jsonify(new_city.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'])
+@app_views.route('/cities/<city_id>', methods=['PUT']
+                 strict_slashes=False)
 def put_city(city_id):
     """Update `city` object"""
     city = storage.get(City, city_id)
